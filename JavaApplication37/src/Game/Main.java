8dae975c53d07c6991eef5e9ada832879b493ec7
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-05-20 13:23:40 +0300

    Add 1 and 2 numbers in random position inside class newNumberO.

--- JavaApplication37/src/Game/Main.java
+++ JavaApplication37/src/Game/Main.java
@@ -13,6 +13,9 @@ package Game;
 public class Main {
     public static void main(String[] args){
         Game game = new Game();
+        for(int i =0;i<11;i++){
+        game.addNewNumber();
         game.printArray();
+        }
     }
 }
